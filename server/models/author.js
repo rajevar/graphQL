2a7dc0a29d68be4b02d36ff7a3a489d1ed6dbddf
Rajeev Ravindran <rajevar@gmail.com>
2020-04-11 23:47:14 -0700

    removing gitignore files

--- server/models/author.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const mSchema = mongoose.Schema;
-
-const authorSchema = new mSchema({
-  name: String,
-  age: Number
-});
-
-module.exports = mongoose.model("Author", authorSchema);
